cf56b779f7dc53714b2757f19b1c630f94db17b2
Joseph Calla <35270796+JosephCalla@users.noreply.github.com>
2022-12-08 15:29:28 -0500

    Graphs part 1

--- 36-graphs/projects/starter/Graph.playground/Contents.swift
+++ 36-graphs/projects/starter/Graph.playground/Contents.swift
@@ -1,6 +1,50 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
-public enum EdType {
+import Foundation
+
+// Vertices = Circles
+// Edges = lines that connect them
+
+// Weight = it that represents the cost of using this edge.
+// Type Lines
+// Directed   =  -------->
+// UnDirected =  <------->
+
+public enum EdgeType {
+    case directed
+    case undirected
+}
+
+public protocol Graph {
+    associatedtype Element
     
+    // Creates a vertex and adds it to the graph
+    func createVertex(data: Element) -> Vertex<Element>
+    // Adds a directed edge between two vertices
+    func addDirectedEdge(
+        from source: Vertex<Element>,
+        to destination: Vertex<Element>,
+        weight: Double?
+    )
+    // Adds an undirected (or bi-directional) edge between two vertices
+    func addUndirectedEdge(
+        between source: Vertex<Element>,
+        and destination: Vertex<Element>,
+        weight: Double?
+    )
+    // Uses "EdgeType" to add either a directed or undirected edge between two vertices
+    func add(
+        _ edge: EdgeType,
+        from source: Vertex<Element>,
+        to destination: Vertex<Element>,
+        weight: Double?
+    )
+    // Returns a list of outgoing edges from a specific vertex
+    func edges(from source: Vertex<Element>) -> [Edge<Element>]
+    // Returns the weight of the edge between two vertices
+    func weight(
+        from source: Vertex<Element>,
+        to destination: Vertex<Element>
+    ) -> Double?
 }
---
+++ 36-graphs/projects/starter/Graph.playground/Pages/Graph.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+let graph = AdjacencyList<String>()
+
+let singapore = graph.createVertex(data: "Singapore")
+let tokyo = graph.createVertex(data: "Tokyo")
+let hongKong = graph.createVertex(data: "Hong Kong")
+let detroit = graph.createVertex(data: "Detroit")
+let sanFrancisco = graph.createVertex(data: "San Francisco")
+let washingtonDC = graph.createVertex(data: "Washington DC")
+let austinTexas = graph.createVertex(data: "Austin Texas")
+let seattle = graph.createVertex(data: "Seattle")
+
+graph.add(.undirected, from: singapore, to: hongKong, weight: 300)
+graph.add(.undirected, from: singapore, to: tokyo, weight: 500)
+graph.add(.undirected, from: hongKong, to: tokyo, weight: 250)
+graph.add(.undirected, from: tokyo, to: detroit, weight: 450)
+graph.add(.undirected, from: tokyo, to: washingtonDC, weight: 300)
+graph.add(.undirected, from: hongKong, to: sanFrancisco, weight: 600)
+graph.add(.undirected, from: detroit, to: austinTexas, weight: 50)
+graph.add(.undirected, from: austinTexas, to: washingtonDC, weight: 292)
+graph.add(.undirected, from: sanFrancisco, to: washingtonDC, weight: 337)
+graph.add(.undirected, from: washingtonDC, to: seattle, weight: 277)
+graph.add(.undirected, from: sanFrancisco, to: seattle, weight: 218)
+graph.add(.undirected, from: austinTexas, to: sanFrancisco, weight: 297)
+
+print(graph)
+
+
+let number = 16.0
+print("\(number) squared is \(number * number), and its square root is \(sqrt(number))")
+
+
+struct Spaceship {
+    var name: String {
+        willSet {
+            print("I'm called \(newValue)!")
+        }
+    }
+}
+
+var serenity = Spaceship(name: "Serenity")
+serenity.name = "TARDIS"
